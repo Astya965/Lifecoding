a7f278b40094ea69243f77b2411e3e1c68983722
astya965 <astya965@gmail.com>
2022-11-24 11:43:33 +0100

    feat: Add fake cycled list (repeat one time)

--- utils/mock/linkedList.ts
+++ utils/mock/linkedList.ts
@@ -13,3 +13,13 @@ export const linkedListFromArray = (arr: number[], i: number = 0): ListNode | nu
     }
     return null;
 };
+
+// TO DO Find way to create list with real cycle
+export const linkedListWithFakeCycleFromArray = (arr: number[], i: number = 0, cycled = false) => {
+    if (i < arr.length) {
+        return new ListNode(arr[i], linkedListWithFakeCycleFromArray(arr, i + 1, cycled));
+    } else if (!cycled) {
+        return new ListNode(arr[0], linkedListWithFakeCycleFromArray(arr, 0, true));
+    }
+    return null;
+};
